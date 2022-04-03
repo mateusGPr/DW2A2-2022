3660e3712675160042c4af63d6f4f4d68c9bafae
MateusGPe <mateus.pereira1@aluno.ifsp.edu.br>
2022-04-03 17:04:15 -0300

    Aula 3

---
+++ atividades/A3/js/script.js
@@ -0,0 +1,12 @@
+const Modal = {
+    Open() {
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+    Close() {
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+
+const Transaction = {
+    
+}
